f33a2f7bd0506a964e266f1ae1aa6b5317ab766f
xidios <47945350+xidios@users.noreply.github.com>
2021-06-14 17:36:19 +0700

    Добавлены чекпоинты

---
+++ Assets/C# Scripts/PlayerPos.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameMaster gm;
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+}
