d7453dd3ecbf53c7c39da8af7ca9f58cdda94761
xidios <47945350+xidios@users.noreply.github.com>
2021-06-19 00:10:23 +0700

    АлертОбжекты теперь исчезают

--- Assets/C# Scripts/UIAppear.cs
+++ Assets/C# Scripts/UIAppear.cs
@@ -8,18 +8,19 @@ public class UIAppear : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Canvas canvas;
-    
     [SerializeField] string textField;
-
+    [SerializeField] SpriteRenderer sp;
+    private bool flag = true;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && flag)
         {
             print("Working");
             canvas.enabled = true;
             text.text = textField;
             text.color = Color.white;
-            
+            sp.enabled = false;
+            flag = false;
         }
     }
     void OnTriggerExit2D(Collider2D other)
