2470c550290604a68d234e4274f333bf71b0e78e
xidios <47945350+xidios@users.noreply.github.com>
2021-07-02 17:15:38 +0700

    ветки падают

---
+++ Assets/C# Scripts/BranchFallTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchFallTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private float delayBeforeFall = 2f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            print("Branch is falling");
+            Invoke("Fall", delayBeforeFall);
+            //Destroy(gameObject, 5f);
+        }
+    }
+    private void Fall()
+    {
+        rb.isKinematic = false;
+    }
+}
